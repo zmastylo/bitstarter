86ff024177919a7b49c126a9955447ed58f00961
Ubuntu <ubuntu@ip-172-31-18-122.us-west-2.compute.internal>
2013-07-12 04:51:35 +0000

    Read index.html and send in response

--- web.js
+++ web.js
@@ -3,10 +3,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+    var fs = require('fs');
+    try {
+      /// read index.html and send in response
+      var indexFileContent = fs.readFileSync("index.html");
+      response.send(indexFileContent);
+    }
+    catch(e) {
+      console.log("Error reading index.html");
+    }
+  } 
 });
